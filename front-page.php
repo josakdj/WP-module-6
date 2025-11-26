e51c2b6fdf4ac9c14ec5dfd09e1caf97f4f1a7e4
josakdj <amararifi1234@gmail.com>
2025-11-26 19:05:30 +0100

    Create front-page.php

---
+++ front-page.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+
+<main> </main>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+
+    <section class="hero">
+    <img src="<?php echo esc_url(get_theme_file_uri('images/banner.png')) ?>" alt="site banner">
+    </section>
+
+    <section class="features">
+        <br>
+        <h2 id="heading">Pse me shiku cimen</h2>
+        <br>
+        <li>
+            <img src="<?php echo esc_url(get_theme_file_uri('images/funi.png')) ?>" alt="site banner">
+    <span>Shum fanni</span>
+        </li>
+
+                <li>
+            <img src="<?php echo esc_url(get_theme_file_uri('images/qumili.png')) ?>" alt="site banner">
+    <span>shok me qumilin</span>
+        </li>
+
+                <li>
+            <img src="<?php echo esc_url(get_theme_file_uri('images/leci.png')) ?>" alt="site banner">
+    <span>dost e ka lecin</span>
+        </li>
+
+
+
+    </main>
+</body>
+</html>
