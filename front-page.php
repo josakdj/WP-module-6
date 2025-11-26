0cf74eaaba468ddb2646ea570791e3168a9c09ae
josakdj <amararifi1234@gmail.com>
2025-11-26 19:20:05 +0100

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -35,7 +35,28 @@
     <span>dost e ka lecin</span>
         </li>
 
+<section>
+    <br>
 
+    <section class="section-pad">
+        <h6 id="curricula-heading">Cima</h6>
+        <ul class="curricula-grid">
+
+    <li class="card">
+        <h3 class="card-title">
+            <span class="label">Cima Fani</span>
+            <span class="label">Cima Dhe Hasan Kungji</span>
+        </h3>
+        <ul class="class=points">
+            <li><strong>Humori:</strong>10/10</li>
+            <li><strong>Videot:</strong>10/10</li>
+            <li><strong>Humor me shok:</strong>10/10</li>
+            <li><strong>legjend:</strong>shum</li>
+        </ul>
+    </li>
+
+        </ul>
+    </section>
 
     </main>
 </body>
